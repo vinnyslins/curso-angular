231ae05e6d2cc3c364a2f79df729c83014648def
Vinnys Lins <vlins@orion.tech>
2018-10-01 16:23:08 -0300

    Adding SCSS style on TopoComponent

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-topo',
-  templateUrl: './topo.component.html'
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.scss']
 })
 export class TopoComponent {}
