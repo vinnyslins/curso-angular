cb4df92ecc9858a779be48e9b4162d9d47b6c91a
Vinnys Lins <vlins@orion.tech>
2018-10-02 16:38:34 -0300

    Implemented string interpolation and property binding

--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tentativas.component.scss']
 })
 export class TentativasComponent implements OnInit {
+  public coracaoVazio = 'assets/coracao_vazio.png';
+  public coracaoCheio = 'assets/coracao_cheio.png';
 
   constructor() { }
 
