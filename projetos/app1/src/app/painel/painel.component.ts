c236a5a5cebf455619b7f21a6d6ae7fa87111259
Vinnys Lins <vlins@orion.tech>
2018-10-02 17:04:24 -0300

    Implementend Frase class and mock with phrase constants

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
 
 @Component({
   selector: 'app-painel',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./painel.component.scss']
 })
 export class PainelComponent implements OnInit {
+  public frases: Frase[] = FRASES;
+  public instrucao = 'Traduza a frase:';
 
   constructor() { }
 
