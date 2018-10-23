cd4a8e45c6b10876f8502431ab7f7c12ad004bd3
Vinnys Lins <vlins@orion.tech>
2018-10-23 11:48:06 -0300

    Controlling the attempts

--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Coracao } from '../shared/coracao.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { Coracao } from '../shared/coracao.model';
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.scss']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
   @Input() public tentativas: number;
 
   public coracoes: Coracao[] = [
@@ -17,7 +17,13 @@ export class TentativasComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    if (this.tentativas !== this.coracoes.length) {
+      const index = this.coracoes.length - this.tentativas - 1;
+      this.coracoes[index].cheio = false;
+    }
   }
 
+  ngOnInit() {
+  }
 }
