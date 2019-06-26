be328597a61415fb7d113d81618ef27aefccffb9
Vinnys Lins <vinnysfl@hotmail.com>
2019-06-25 23:49:56 -0300

    Implementing data binding in the oferta list

--- projetos/app2/src/app/shared/oferta.model.ts
+++ projetos/app2/src/app/shared/oferta.model.ts
@@ -2,7 +2,7 @@ export class Oferta {
   public id: number;
   public categoria: string;
   public titulo: string;
-  public descricaoOferta: string;
+  public descricao: string;
   public anunciante: string;
   public valor: number;
   public destaque: boolean;
